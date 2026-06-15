68c04ad10b5b0d74b834c48ef923318bddbbcaea
byonesheart <936527867@qq.com>
2026-06-15 23:03:57 +0800

    配置和数据文件改存到 %APPDATA%\AutoBackup"

--- core/config.py
+++ core/config.py
@@ -22,10 +22,7 @@ DEFAULT_CONFIG = {
     }
 }
 
-if getattr(sys, "frozen", False):
-    BASE_DIR = os.path.dirname(sys.executable)
-else:
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(os.environ.get("APPDATA", os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "AutoBackup")
 CONFIG_PATH = os.path.join(BASE_DIR, "config.json")
 LOG_PATH = os.path.join(BASE_DIR, "backup_log.json")
 
